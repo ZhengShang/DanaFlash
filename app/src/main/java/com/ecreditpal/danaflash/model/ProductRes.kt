2bc49b28497cceb9d852fdedfd6cedf9381aa8b1
ZhengShang <shang10023@126.com>
2021-01-11 22:32:28 +0800

    update

--- app/src/main/java/com/ecreditpal/danaflash/model/ProductRes.kt
+++ app/src/main/java/com/ecreditpal/danaflash/model/ProductRes.kt
@@ -1,5 +1,7 @@
 package com.ecreditpal.danaflash.model
 
+import java.math.BigDecimal
+
 data class ProductRes(
     val isHomeOrder: Boolean?, // true
     val list: List<Product>?,
@@ -7,27 +9,27 @@ data class ProductRes(
     val selectIds: List<Int?>?
 ) {
     data class Product(
-        val amountMax: Double?, // 1000000.00
-        val amountMin: Double?, // 600000.00
+        val amountMax: BigDecimal?, // 1000000.00
+        val amountMin: BigDecimal?, // 600000.00
         val api: Boolean?, // false
         val applyNum: Int?, // 2226
-        val auditScore: Any?, // null
+        val auditScore: String?, // null
         val canApply: Boolean?, // true
         val id: Int?, // 32
         val image: String?, // /staging/MemberData/4919bf58c4a94d77b0ef7b8cf663b29d
-        val interestMax: Any?, // null
-        val interestMin: Double?, // 0.330
+        val interestMax: BigDecimal?, // null
+        val interestMin: BigDecimal?, // 0.330
         val link: String?, // https://app.appsflyer.com/com.julofinance.juloapp?c={campaign_name}&af_siteid={af_siteid}&pid=kspid_int&clickid={clickid}&android_id={android_id}&advertising_id={advertising_id}
         val name: String?, // Julo
-        val packageName: Any?, // null
-        val periodMax: Any?, // null
-        val periodMin: Any?, // null
-        val periodUnit: Any?, // null
-        val processTime: Double?, // 24.0
-        val score: Double?, // 4.6
+        val packageName: String?, // null
+        val periodMax: Int?, // null
+        val periodMin: Int?, // null
+        val periodUnit: Int?, // null
+        val processTime: BigDecimal?, // 24.0
+        val score: BigDecimal?, // 4.6
         val spareLink: String?,
         val summary: String?,
-        val tag: Any?, // null
-        val xinghe: Any? // null
+        val tag: String?, // null
+        val xinghe: Int? // null
     )
 }
