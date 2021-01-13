1bbfaf5fbb9b76807a2ae2098f533ccf18930502
zhengshang <zhengshang@mucang.cn>
2021-01-13 18:58:46 +0800

    orders 1

---
+++ app/src/main/java/com/ecreditpal/danaflash/ui/orders/MyOrdersFragment.kt
@@ -0,0 +1,17 @@
+package com.ecreditpal.danaflash.ui.orders
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ecreditpal.danaflash.base.BaseFragment
+
+class MyOrdersFragment : BaseFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/ecreditpal/danaflash/ui/orders/OrdersActivity.kt
@@ -0,0 +1,9 @@
+package com.ecreditpal.danaflash.ui.orders
+
+import com.ecreditpal.danaflash.R
+import com.ecreditpal.danaflash.base.BaseNavActivity
+
+class OrdersActivity : BaseNavActivity() {
+    override fun navGraphId() = R.navigation.orders_navigation
+
+}
