c4d231baf68579d67f463f10024bd6256aef5d03
zhengshang <zhengshang@mucang.cn>
2021-01-20 16:49:23 +0800

    faq

---
+++ app/src/main/java/com/ecreditpal/danaflash/ui/faq/FaqActivity.kt
@@ -0,0 +1,8 @@
+package com.ecreditpal.danaflash.ui.faq
+
+import com.ecreditpal.danaflash.R
+import com.ecreditpal.danaflash.base.BaseNavActivity
+
+class FaqActivity : BaseNavActivity() {
+    override fun navGraphId() = R.navigation.faq_navigation
+}
---
+++ app/src/main/java/com/ecreditpal/danaflash/ui/faq/FaqFragment.kt
@@ -0,0 +1,7 @@
+package com.ecreditpal.danaflash.ui.faq
+
+import com.ecreditpal.danaflash.base.BaseFragment
+
+class FaqFragment : BaseFragment() {
+
+}
